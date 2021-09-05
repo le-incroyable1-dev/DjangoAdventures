6572c873762c9d6347677ad1d4afeeb0e7039488
Aurav <aurav.tomar@gmail.com>
2021-09-06 00:49:19 +0530

    push changes in urls and views

--- DjangoWebProject1/polls/urls.py
+++ DjangoWebProject1/polls/urls.py
@@ -2,8 +2,19 @@ from django.urls import path
 
 from . import views
 
+#add namespace
+app_name = 'polls'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
 
 #if somebody goes to an empty path in ur website open up views' index function
--- DjangoWebProject1/polls/views.py
+++ DjangoWebProject1/polls/views.py
@@ -4,7 +4,11 @@ from django.http import HttpResponse
 #Create your views here
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # - sign shows the reverse according to publication date to show recent ones first !
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    # output = "Welcome to polls !"
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
