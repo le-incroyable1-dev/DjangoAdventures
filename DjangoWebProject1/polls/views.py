aeaab098a401fdcd34db7958b4575dd5de7d14a0
Aurav <aurav.tomar@gmail.com>
2021-09-07 14:21:36 +0530

    add a form to allow a user to vote for a question

--- DjangoWebProject1/polls/views.py
+++ DjangoWebProject1/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
 
 #Create your views here
 
@@ -30,4 +31,5 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+
