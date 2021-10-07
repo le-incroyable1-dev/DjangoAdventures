0eac5846e9c8d324ac4e5a81526f847a41927dc9
Aurav <aurav.tomar@gmail.com>
2021-10-07 21:41:54 +0530

    comments

--- DjangoWebProject1/polls/urls.py
+++ DjangoWebProject1/polls/urls.py
@@ -8,11 +8,15 @@ app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
+
     # ex: /polls/5/
+    # the 'name' value as called by the {% url %} template tag
     path('<int:question_id>/', views.detail, name='detail'),
+
     # ex: /polls/5/results/
     # int:question_id passes the question id (5 above) to views.detail
     path('<int:question_id>/results/', views.results, name='results'),
+
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
