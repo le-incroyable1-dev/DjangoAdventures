b80dfabc71ccb5d45496d8807fcfbebebd9176c9
Aurav <aurav.tomar@gmail.com>
2021-10-09 18:35:04 +0530

    setting up results

--- DjangoWebProject1/polls/views.py
+++ DjangoWebProject1/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+
 from .models import Question, Choice
 
 #Create your views here
@@ -26,10 +27,21 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id);
+    return render(request, 'polls/results.html', {'question' : question})
 
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = Question.choice_set.get(pk = request.POST['choice']);
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question' : question,
+            'error_message' : "You did not select a choice!",
+            })
+    else:
+        selected_choice.votes+=1;
+        selected_choice.save();
 
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,))
