f1af9a82e986df6f817c63598decf43da28503f2
Aurav <aurav.tomar@gmail.com>
2021-10-11 20:06:07 +0530

    setup polls:results

--- DjangoWebProject1/polls/urls.py
+++ DjangoWebProject1/polls/urls.py
@@ -2,22 +2,15 @@ from django.urls import path
 
 from . import views
 
-# add namespace for polls here
 app_name = 'polls'
 
 urlpatterns = [
-    # ex: /polls/
     path('', views.index, name='index'),
 
-    # ex: /polls/5/
-    # the 'name' value as called by the {% url %} template tag
     path('<int:question_id>/', views.detail, name='detail'),
 
-    # ex: /polls/5/results/
-    # int:question_id passes the question id (5 above) to views.detail
     path('<int:question_id>/results/', views.results, name='results'),
 
-    # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
 
