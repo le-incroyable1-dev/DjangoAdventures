43d56c5167e0602e9e07f342f3ed5d69622ef6cc
Aurav <aurav.tomar@gmail.com>
2021-10-11 21:48:48 +0530

    apply generic views

--- DjangoWebProject1/polls/views.py
+++ DjangoWebProject1/polls/views.py
@@ -2,33 +2,29 @@ from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 #Create your views here
 
-def index(request):
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
 
-    # - sign shows the reverse according to publication date to show recent ones first !
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # first 5 questions in the list
+    context_object_name = 'latest_question_list'
+    #context for the list view to be rendered
 
-    output = ', '.join([q.question_text for q in latest_question_list])
-    # list comprehensions in python
-
-    context = {'latest_question_list': latest_question_list}
-    # stuff for front end
-    return render(request, 'polls/index.html', context)
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-   question = get_object_or_404(Question, pk=question_id)
-   return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template = 'polls/details.html'
 
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id);
-    return render(request, 'polls/results.html', {'question' : question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template = 'polls/results.html'
 
 
 def vote(request, question_id):
